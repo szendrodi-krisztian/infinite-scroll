db5b7715593a36923f8e412e491a980a55ad3533
szendrodi-krisztian <szendrodi.krisztian@gmail.com>
2023-02-10 20:48:30 +0100

    Initial

---
+++ Assets/InfiniteScroll/Scripts/InfiniteScrollView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rabbit.UI
+{
+    public class InfiniteScrollView : MonoBehaviour
+    {
+    
+    }
+    
+    public interface IScrollContentProvider {
+    }
+}
